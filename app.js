bf0ecb38cd7300bc89ea3e5b553f6d799fc6d374
AZC <albertzcheng@gmail.com>
2020-12-31 11:48:04 -0800

    code work until getting to the page of Sales Navigator, then it says the document is not found

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const puppeteer = require('puppeteer');
-let company = "";
-
-const urlEncodingConvert = (company) => {
-    const uri = `https://www.linkedin.com/sales/search/people?companyIncluded=${company}&companyTimeScope=CURRENT&doFetchHeroCard=false&logHistory=true&page=1&relationship=F%2CS`;
-    return encodeURI(uri);
-}
-
-//console.log(urlEncodingConvert(company));
+//test company: Stanley Black and Decker
+let company = "Stanley%2520Black%2520%2526%2520Decker";
 
 const secondConnect = async (company) => {
 
     //Launch and setup
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+    let links = [];
+
     await page.goto('https://www.linkedin.com/');
 
     await page.type('#session_key', '');
@@ -20,25 +16,50 @@ const secondConnect = async (company) => {
 
     await page.click('.sign-in-form__submit-button');
 
-    // await page.waitForNavigation().then(() => {
-    //     console.log("Wait for entering phone code")
-    // }); // <------------------------- Wait for Navigation
-
-    await page.waitForSelector('#ember20', 60000).then(() => {
+    //Wait for it to ask for text code due to 2 way authentication
+    await page.waitForSelector('#ember20', { timeout: 60000 }).then(() => {
         console.log("I'm in");
     });
 
+    //Now that there is the authentication, go to Sales Navigator on LinkedIn
     await page.goto(`https://www.linkedin.com/sales/search/people?companyIncluded=${company}&companyTimeScope=CURRENT&doFetchHeroCard=false&logHistory=true&page=1&relationship=F%2CS`);
 
-    await page.waitForSelector('#ember48', 60000).then(() => {
+    //Wait until the search page is loaded 
+    await page.waitForSelector('#ember48', { timeout: 60000 }).then(() => {
         console.log("Search Successful");
+        let extracts = document.querySelectorAll('.search-results__result-item');
+        let peopleHeader = "http://www.linkedin.com/sales/people/";
+        //let pgCount = Math.ceil(parseInt(document.querySelector('#search-spotlight-tab-ALL').firstChild.innerText) / 25);
+
+        extracts.forEach(item => {
+            links.push(peopleHeader.concat(item.getAttribute("data-scroll-into-view").slice(24, -1)))
+        });
+        console.log(links);
     });
 
-    let allElements = await page.$$('.search-results__result-item');
-    console.log(JSON.stringify(allElements.JSON()));
-    //await page.goto(`https://www.linkedin.com/sales/search/people?companyIncluded=${company}&companyTimeScope=CURRENT&doFetchHeroCard=false&logHistory=true&page=2&relationship=F%2CS`);
+    //Code below does not work----------------------------------------------------------------
+    let extracts = document.querySelectorAll('.search-results__result-item');
+    let peopleHeader = "http://www.linkedin.com/sales/people/";
+    //let pgCount = Math.ceil(parseInt(document.querySelector('#search-spotlight-tab-ALL').firstChild.innerText) / 25);
 
-    // 
+    extracts.forEach(item => {
+        links.push(peopleHeader.concat(item.getAttribute("data-scroll-into-view").slice(24, -1)))
+    });
+    console.log(links);
+
+    // for (let i = 2; i < pgCount; i++) {
+
+    //     await page.goto(`https://www.linkedin.com/sales/search/people?companyIncluded=${company}&companyTimeScope=CURRENT&doFetchHeroCard=false&logHistory=true&page=${i}&relationship=F%2CS`);
+
+    //     //let peopleHeader = "http://www.linkedin.com/sales/people/";
+    //     await page.waitForNavigation().then(() => {
+
+    //         extracts.forEach(item => {
+    //             links.push(peopleHeader.concat(item.getAttribute("data-scroll-into-view").slice(24, -1)))
+    //         });
+    //     });
+
+    // }
     //await browser.close();
 };
 
--- test.js
+++ test.js
@@ -1,2 +1,22 @@
 let e = document.querySelectorAll('.search-results__result-item');
-console.log(e);
+let links = [];
+let peopleHeader = "http://www.linkedin.com/sales/people/";
+//get a string with the link;
+e.forEach(item => { links.push(item.getAttribute("data-scroll-into-view")) });
+
+links = links.forEach(item => { item = peopleHeader.concat(item) });
+console.log(links);
+
+let extracts = document.querySelectorAll('.search-results__result-item');
+let links = [];
+let peopleHeader = "http://www.linkedin.com/sales/people/";
+extracts.forEach(item => {
+    links.push(peopleHeader.concat(item.getAttribute("data-scroll-into-view").slice(24, -1)))
+})
+
+//Get the pages
+let pgCount = Math.ceil(parseInt(document.querySelector('#search-spotlight-tab-ALL').firstChild.innerText) / 25);
+for (let i = 2; i < pgCount; i++) {
+    await page.goto(`https://www.linkedin.com/sales/search/people?companyIncluded=${company}&companyTimeScope=CURRENT&doFetchHeroCard=false&logHistory=true&page=${i}&relationship=F%2CS`);
+}
+
